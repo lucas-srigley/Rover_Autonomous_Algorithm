7c7a1ee4e59dc22c5756fcf2d280f23120fc0ea4
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-18 21:19:22 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -14,7 +14,9 @@ def main():
         from sensor_msgs.msg import LaserScan
         for dist in rover.laser_distances:
             if dist < 0.5:
-                print("TOO CLOSE")
+                    left_side_speed = 0
+                    right_side_speed = 0
+                #print("TOO CLOSE")
         rover.send_command(left_side_speed, right_side_speed)
         i = i + 1
         sleep(0.01)
